6029f8363e7e27f05208a633567d1e0b86d1153e
tothtamas9 <ttamas026@gmail.com>
2023-11-13 17:49:08 +0100

    Add project files.

---
+++ balkezesek/Balkezes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    public class Balkezes
+    {
+        string nev;
+        string elso;
+        string utolso;
+        int suly;
+        int magassag;
+
+        public Balkezes(string nev, string elso, string utolso, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elso = elso;
+            this.utolso = utolso;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev; }
+
+        public int ElsoEv { get => int.Parse(elso.Substring(0,4)); }
+        public int ElsoHonap { get => int.Parse(elso.Substring(5,2)); }
+        public int ElsoNap { get => int.Parse(elso.Substring(8,2)); }
+
+        public int UtolsoEv { get => int.Parse(utolso.Substring(0, 4)); }
+        public int UtolsoHonap { get => int.Parse(utolso.Substring(5, 2)); }
+        public int UtolsoNap { get => int.Parse(utolso.Substring(8, 2)); }
+
+        public int Suly { get => suly; }
+        public int Magassag { get => magassag; }
+    }
+}
---
+++ balkezesek/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. feladat:
+            List<Balkezes> balkezesek = new List<Balkezes>();
+            string[] olvasottBalkezesek = File.ReadAllLines("Sources\\balkezesek.csv");
+
+            for (int i = 1; i < olvasottBalkezesek.Length; i++)
+            {
+                string[] tagok = olvasottBalkezesek[i].Split(';');
+                Balkezes ujBalkezes = new Balkezes(tagok[0], tagok[1], tagok[2], int.Parse(tagok[3]), int.Parse(tagok[4]));
+                balkezesek.Add(ujBalkezes);
+            }
+
+            // 3. feladat:
+            Console.WriteLine($"3. feladat: {balkezesek.Count}");
+
+            // 4. feladat:
+            Console.WriteLine("4. feladat:");
+            foreach (var i in balkezesek)
+            {
+                if (i.UtolsoEv == 1999 && i.UtolsoHonap == 10)
+                {
+                    Console.WriteLine($"\t{i.Nev}: {Math.Round(i.Magassag * 2.54, 1)} cm");
+                }
+            }
+
+            //5.feladat:
+            Console.WriteLine("5. feladat:");
+            int evszam = 0;
+            do
+            {
+                Console.Write("Kérek egy 1990 és 1999 közötti számot!: ");
+                evszam = Convert.ToInt32(Console.ReadLine());
+                if (1990 <= evszam && evszam <= 1999)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Hibás adat!");
+                }
+
+            } while (true);
+
+            //6. feladat:
+            Console.WriteLine(Math.Round(balkezesek.Where(x => x.ElsoEv <= evszam && x.UtolsoEv >= evszam).Average(x => x.Suly), 2));
+        }
+    }
+}
